dc6d138c60810f3370ac5123aadbc77b9433828c
yanggaofeng <redppl@hotmail.com>
2025-04-22 18:23:32 +0800

    update

--- use-redis/src/main/java/io/github/redexpress/use_redis/UseRedisApplication.java
+++ use-redis/src/main/java/io/github/redexpress/use_redis/UseRedisApplication.java
@@ -1,11 +1,24 @@
 package io.github.redexpress.use_redis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
-public class SimpleApplication {
+public class UseRedisApplication implements CommandLineRunner {
     public static void main(String[] args) {
-        SpringApplication.run(SimpleApplication.class, args);
+        SpringApplication.run(UseRedisApplication.class, args);
+    }
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+        redisTemplate.opsForValue().set("name", "YANG Gaofeng");
+        String name = redisTemplate.opsForValue().get("name").toString();
+        System.out.println(name);
     }
 }
