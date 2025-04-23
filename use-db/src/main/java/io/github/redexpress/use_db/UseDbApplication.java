34fb8d1a3bcd63755ee5d60f7b7e9febc53cad39
Gavin Yang <redppl@hotmail.com>
2025-04-23 14:13:08 +0800

    app

---
+++ use-db/src/main/java/io/github/redexpress/use_db/UseDbApplication.java
@@ -0,0 +1,11 @@
+package io.github.redexpress.use_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseDbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UseDbApplication.class, args);
+    }
+}
---
+++ use_websocket/src/main/java/io/github/redexpress/use_websocket/WebsocketApplication.java
@@ -0,0 +1,6 @@
+package io.github.redexpress.use_websocket;
+
+public class WebsocketApplication {
+    public static void main(String[] args) {
+    }
+}
